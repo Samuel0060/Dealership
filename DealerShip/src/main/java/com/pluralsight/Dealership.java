7e011b7b285fef07cd6aa4fa48dc46a24f0eb91f
Samuel Stephenson <sstephenson10469@gmail.com>
2025-05-12 13:38:18 -0400

    coded version of addVehicle

--- DealerShip/src/main/java/com/pluralsight/Dealership.java
+++ DealerShip/src/main/java/com/pluralsight/Dealership.java
@@ -7,15 +7,50 @@ public class Dealership {
     private String name;
     private String address;
     private String phone;
+    private ArrayList<Vehicle> inventory;
 
-    ArrayList<Vehicle> Inventory = new ArrayList<>();
 
     public Dealership(String name, String address, String phone) {
         this.name = name;
         this.address = address;
         this.phone = phone;
+        this.inventory = new ArrayList<>();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Vehicle> inventory) {
+        this.inventory = inventory;
+    }
+
+
+    //methods
     public ArrayList<Vehicle> getVehiclesByPrice(ArrayList<Vehicle> inventory, double min, double max) {
         return null;
     }
--- DealerShip/src/main/java/com/pluralsight/DealershipFileManager.java
+++ DealerShip/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class DealershipFileManager {
+    public Dealership getDealership () {
+        Dealership dealership = new Dealership(name, address, phone);
+        return dealership;
+    }
+
+    public void saveDealership()
 }
